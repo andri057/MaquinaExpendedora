c4bea8ecfa1fc964f41d03c48bd0ae702130d705
wilmermoran <wilmer19427@gmail.com>
2022-09-06 12:58:04 -0400

    improvement:
    -refactor code.

--- app.js
+++ app.js
@@ -1,113 +1,50 @@
 
+function maquinaExpendedora() {
+    const producto = prompt("ESCOJE LA BEBIDA QUE NECESITAS: \n 1.AGUA PLANETA AZUL \n 2.COCACOLA \n 3.KOLA REAL \n 4.PEPSI \n 5.COUNTRY CLUB \n 6.SEVEN UP \n 7.SALIR")
 
-
-
-let producto = prompt("ESCOJE LA BEBIDA QUE NECESITAS: 1.AGUA PLANETA AZUL 2.COCACOLA 3.KOLA REAL 4.PEPSI 5.COUNTRY CLUB 6.SEVEN UP")
-
-if(producto.toLowerCase() == 1){
-let respuesta = prompt("LA BEBIDA QUE ESCOGIO ES AGUA PLANETA AZUL ES CORRECTO? RESPONDA 1 SI ES 'SI' Y 2 SI ES 'NO'")
-    if(respuesta == 1 ){
-        
-            console.log("que tenga buen dia y disfrute su bebida")
-        
+    switch(producto) {
+        case '1':
+            buyADrink('AGUA PLANETA AZUL', 0);
+            break;
+        case '2':
+            buyADrink('COCA-COLA', 25);
+            break;
+        case '3':
+            buyADrink('KOLA-REAL', 20);
+            break;
+        case '4':
+            buyADrink('PEPSI', 25)
+            break;
+        case '5':
+            buyADrink('COUNTRY-CLUB', 30);
+            break;
+        case '6':
+            buyADrink('SEVEN-UP', 20);
+            break;
+        default:
+            alert("Tenga un buen dia");
     }
-    if(respuesta == 2){
+}
 
-        alert("por favor reinice vuelva a intentarlo")
-    }
-    if(respuesta > 2 ){
-    alert("la respuesta no es ninguna de las opociones")
-    }
-    }
-    if(producto.toLowerCase() == 2){
-        let respuesta = prompt("LA BEBIDA QUE ESCOGIO ES COCACOLA ES CORRECTO? RESPONDA 1 SI ES 'SI' Y 2 SI ES 'NO'")
-            if(respuesta == 1 ){
-                let dinero = prompt("INGRESE EL DINERO")
-                if(dinero >= 25){
-                    dinero = dinero - 25
-                    console.log("su cambio es", dinero, "que tenga buen dia y disfrute su bebida")
-                }else{
-                    alert("el monto ingresado no es el indicado por favor reinicie y vuelva a intentar")
-                }
-            }
-            if(respuesta == 2){
-                alert("por favor reinice vuelva a intentarlo")
-            }
-            if(respuesta > 2 ){
-            alert("la respuesta no es ninguna de las opociones")
-            }
-            }
-            
-     if(producto.toLowerCase() == 3){
-         let respuesta = prompt("LA BEBIDA QUE ESCOGIO ES KOLA REAL ES CORRECTO? RESPONDA 1 SI ES 'SI' Y 2 SI ES 'NO'")
-              if(respuesta == 1 ){
-                        let dinero = prompt("INGRESE EL DINERO")
-                  if(dinero >= 20){
-                    dinero = dinero - 20
-                    console.log("su cambio es", dinero, "que tenga buen dia y disfrute su bebida")
-                   }else{
-                    alert("el monto ingresado no es el indicado por favor reinicie y vuelva a intentar")
-                 }
-            }
-            if(respuesta == 2){
-                   alert("por favor reinice vuelva a intentarlo")
-            }
-            if(respuesta > 2 ){
-            alert("la respuesta no es ninguna de las opociones")
-                    }
-            }
-     if(producto.toLowerCase() == 4){
-         let respuesta = prompt("LA BEBIDA QUE ESCOGIO ES PEPSI ES CORRECTO? RESPONDA 1 SI ES 'SI' Y 2 SI ES 'NO'")
-                if(respuesta == 1 ){
-                               let dinero = prompt("INGRESE EL DINERO")
-                         if(dinero >= 25){
-                           dinero = dinero - 25
-                           console.log("su cambio es", dinero, "que tenga buen dia y disfrute su bebida")
-                          }else{
-                           alert("el monto ingresado no es el indicado por favor reinicie y vuelva a intentar")
-                        }
-                   }
-                   if(respuesta == 2){
-                          alert("por favor reinice vuelva a intentarlo")
-                   }
-                   if(respuesta > 2 ){
-                   alert("la respuesta no es ninguna de las opociones")
-                           }
-                   }
+function buyADrink(drink, price) {
+    let respuesta = prompt(`LA BEBIDA QUE ESCOGIO ES ${drink} ES CORRECTO? RESPONDA 1 SI ES 'SI' Y 2 SI ES 'NO'`)
 
-                   if(producto.toLowerCase() == 5){
-                    let respuesta = prompt("LA BEBIDA QUE ESCOGIO ES COUNTRY CLUB ES CORRECTO? RESPONDA 1 SI ES 'SI' Y 2 SI ES 'NO'")
-                           if(respuesta == 1 ){
-                                          let dinero = prompt("INGRESE EL DINERO")
-                                    if(dinero >= 30){
-                                      dinero = dinero - 30
-                                      console.log("su cambio es", dinero, "que tenga buen dia y disfrute su bebida")
-                                     }else{
-                                      alert("el monto ingresado no es el indicado por favor reinicie y vuelva a intentar")
-                                   }
-                              }
-                              if(respuesta == 2){
-                                     alert("por favor reinice vuelva a intentarlo")
-                              }
-                              if(respuesta > 2 ){
-                              alert("la respuesta no es ninguna de las opociones")
-                                      }
-                              }
-                              if(producto.toLowerCase() == 6){
-                                let respuesta = prompt("LA BEBIDA QUE ESCOGIO ES SEVEN UP ES CORRECTO? RESPONDA 1 SI ES 'SI' Y 2 SI ES 'NO'")
-                                       if(respuesta == 1 ){
-                                                      let dinero = prompt("INGRESE EL DINERO")
-                                                if(dinero >= 15){
-                                                  dinero = dinero - 15
-                                                  console.log("su cambio es", dinero, "que tenga buen dia y disfrute su bebida")
-                                                 }else{
-                                                  alert("el monto ingresado no es el indicado por favor reinicie y vuelva a intentar")
-                                               }
-                                          }
-                                          if(respuesta == 2){
-                                                 alert("por favor reinice vuelva a intentarlo")
-                                          }
-                                          if(respuesta > 2 ){
-                                          alert("la respuesta no es ninguna de las opociones")
-                                                  }
-                                          }
+    if (respuesta === '1' ) {
+        let dinero = prompt("INGRESE EL DINERO");
+        if(dinero >= price){
+            dinero = dinero - price
+            console.log("su cambio es", dinero, "que tenga buen dia y disfrute su bebida");
+        }else{
+            alert("el monto ingresado no es el indicado por favor reinicie y vuelva a intentar");
+            maquinaExpendedora();
+        }
+
+    } else if (respuesta === '2'){
+        alert("por favor vuelva a intentarlo");
+        maquinaExpendedora();
+    } else {
+        alert("la respuesta no es ninguna de las opociones");
+    }
+}
+
+maquinaExpendedora();
